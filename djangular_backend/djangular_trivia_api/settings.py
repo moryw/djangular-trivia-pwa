308d902825b6eaab1a3fb3d4feae67f4c15fe3a0
Mory <matt@skitskat.xyz>
2024-08-14 09:32:42 -0600

    refactor(project): restructure project directories
    Renamed project directories and updated settings for directory references. Removed redundant pycache files.
    
    This commit restructures the project directories, updating file paths and references accordingly. Redundant pycache files have been removed.

--- djangular_backend/djangular_trivia_api/settings.py
+++ djangular_backend/djangular_trivia_api/settings.py
@@ -64,7 +64,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
